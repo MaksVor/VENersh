5a9a39d97071749d8115ba7c8775560dc3e36464
инет <vasya.shchelokov@mail.ru>
2020-10-13 15:45:40 +0300

    Prived zemlyane

---
+++ Morosennoe.js
@@ -0,0 +1,9 @@
+let varennik = ['Клубника', 'Сахар', 'Картошка', 'Мясо'];
+let Nog = 0;
+varennik.forEach (function(item, index,array) {
+    console.log(item, index);
+});
+for (let i = 0; i < varennik.length; i++) {
+
+}
+console.log(Nog);
